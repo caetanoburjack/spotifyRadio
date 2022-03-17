87d2442dc0ea4e69eb39cfc80f949c8c6e7b54e8
Caetano Burjack <caetano.burjack@gmail.com>
2022-03-17 09:15:47 -0300

    criando controller e service para lidar com as regras de negocios

--- server/index.js
+++ server/index.js
@@ -2,4 +2,4 @@ import config from './config.js'
 import server from './server.js'
 import { logger } from './util.js'
 // console.log(config)
-server.listen(3000).on('listening', () => logger.info(`server is running at port ${config.port}!!!`))
+server.listen(config.port).on('listening', () => logger.info(`server is running at port ${config.port}!!!`))
